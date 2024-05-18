3bcefed1f3fc31fe40f01650877de7036b4c837f
Anupama Jedi <75876536+anupama003@users.noreply.github.com>
2024-05-18 16:06:39 +0000

    implemented inserting value at end of linked list

--- linkedlist.py
+++ linkedlist.py
@@ -12,6 +12,16 @@ def print_linked_list(head):
     current = current.next
   print("None")
 
+  # Inserting Node at the end
+def insert_at_end(head, value):
+  new_node = Node(value)
+  if head is None:
+    return new_node
+  current = head
+  while current.next is not None:
+    current = current.next
+  current.next = new_node
+  return head
 
 if __name__ == '__main__':
   # Creating nodes
@@ -25,5 +35,15 @@ if __name__ == '__main__':
   node2.next = node3
   node3.next = node4
 
+  # insert into end of linked list
+  insert_at_end(node1, 50)
+  insert_at_end(node1, 60)
+  insert_at_end(node1, 70)
+  insert_at_end(node1, 80)
+  insert_at_end(node1, 90)
+  insert_at_end(node1, 100)
+
   # Printing linked list
   print_linked_list(node1)
+
+
